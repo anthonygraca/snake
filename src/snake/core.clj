328bac933107ce39e83bde25b5650b37361968a8
Anthony Graca <anthonygraca@gmail.com>
2021-11-01 22:09:55 -0700

    added stateful logic to snake game

--- src/snake/core.clj
+++ src/snake/core.clj
@@ -3,48 +3,8 @@
       	   (javax.swing JPanel JFrame Timer JOptionPane)
            (java.awt.event ActionListener KeyListener))
   (:require [clojure.set :refer :all]))
-(defmacro import-static
-  "Imports the named static fields and/or static methods of the class
-  as (private) symbols in the current namespace.
 
-  Example: 
-      user=> (import-static java.lang.Math PI sqrt)
-      nil
-      user=> PI
-      3.141592653589793
-      user=> (sqrt 16)
-      4.0
 
-  Note: The class name must be fully qualified, even if it has already
-  been imported.  Static methods are defined as MACROS, not
-  first-class fns."
-  [class & fields-and-methods]
-  (let [only (set (map str fields-and-methods))
-        the-class (. Class forName (str class))
-        static? (fn [x]
-                    (. java.lang.reflect.Modifier
-                       (isStatic (. x (getModifiers)))))
-        statics (fn [array]
-                    (set (map (memfn getName)
-                              (filter static? array))))
-        all-fields (statics (. the-class (getFields)))
-        all-methods (statics (. the-class (getMethods)))
-        fields-to-do (intersection all-fields only)
-        methods-to-do (intersection all-methods only)
-        make-sym (fn [string]
-                     (with-meta (symbol string) {:private true}))
-        import-field (fn [name]
-                         (list 'def (make-sym name)
-                               (list '. class (symbol name))))
-        import-method (fn [name]
-                          (list 'defmacro (make-sym name)
-                                '[& args]
-                                (list 'list ''. (list 'quote class)
-                                      (list 'apply 'list
-                                            (list 'quote (symbol name))
-                                            'args))))]
-    `(do ~@(map import-field fields-to-do)
-         ~@(map import-method methods-to-do))))
 (import-static java.awt.event.KeyEvent VK_LEFT VK_RIGHT VK_UP VK_DOWN)
 
 (def width 75)
@@ -92,3 +52,24 @@
 
 (defn turn [snake newdir]
   (assoc snake :dir newdir))
+
+;; Mutable state of the Snake game can change in only three ways:
+;; * A game can be reset to its initial state.
+;; * Every turn, the snake updates its position. If it eats an apple, a new apple 
+;;   placed
+;; * A snake can turn.
+(defn reset-game [snake apple]
+  (dosync (ref-set apple (create-apple))
+          (ref-set snake (create-snake)))
+  nil)
+
+(defn update-direction [snake newdir]
+  (when newdir (dosync (alter snake turn newdir))))
+
+(defn update-positions [snake apple]
+  (dosync
+    (if (eats? @snake @apple)
+      (do (ref-set apple (create-apple))
+          (alter snake move :grow))
+      (alter snake move)))
+  nil)
